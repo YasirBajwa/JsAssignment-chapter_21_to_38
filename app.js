f546c7c0854f346cd87e0845ea9c5203f116e839
YasirBajwa <yasirbajwa369@gmail.com>
2020-06-19 12:09:48 +0500

    complete all task from chapter 26 to 30

--- app.js
+++ app.js
@@ -416,7 +416,7 @@
 //--------------------------------------------------------------------------------------------
 //--------------------------------------------------------------------------------------------
 //-------------------------------Chapter 26-30 task 08----------------------------------------
-// 8. Write a program that stores a random secret number from 1 to 10 in a variable.
+//  8. Write a program that stores a random secret number from 1 to 10 in a variable.
 //  Ask the user to input a number between 1 and 10.
 //  If the user input equals the secret number, congratulate the user
 
